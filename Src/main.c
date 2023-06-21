e8e4db01833fc99830d8eb68b9a0fc66c39a8520
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2023-06-21 21:56:14 +0500

    Добавление второй кнопки

--- Src/main.c
+++ Src/main.c
@@ -1,35 +1,35 @@
 /**
-  ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  ******************************************************************************
-  *
-  * COPYRIGHT(c) 2017 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * File Name          : main.c
+ * Description        : Main program body
+ ******************************************************************************
+ *
+ * COPYRIGHT(c) 2017 STMicroelectronics
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f1xx_hal.h"
@@ -41,7 +41,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <math.h>
 
 /* USER CODE END Includes */
@@ -59,19 +59,18 @@ UART_HandleTypeDef huart1;
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
 
-
 /*Y0*******************
-*
-*
-*
-*******************/
+ *
+ *
+ *
+ *******************/
 
-#define DEBUG     false 
+#define DEBUG false
 
-#define X_MIN     1U
-#define X_MAX     319U
-#define Y_MIN     1U
-#define Y_MAX     200U
+#define X_MIN 1U
+#define X_MAX 319U
+#define Y_MIN 1U
+#define Y_MAX 200U
 
 #define TIME_UPDATE 15
 
@@ -80,31 +79,37 @@ int16_t y_snake = 80;
 int16_t old_x = 0;
 int16_t old_y = 0;
 
-int8_t changeX = 0;   //changes the direction of the snake
+int8_t changeX = 0; // changes the direction of the snake
 int8_t changeY = -1;
 /* Параметры еды: */
-int16_t  xFood = 100;
-int16_t  yFood = 100;
+int16_t xFood = 100;
+int16_t yFood = 100;
 int8_t sizeFood = 10;
 
 uint16_t score = 0;
 
-typedef enum {UP, LEFT, DOWN, RIGHT} SPACE_ENUM;
+typedef enum
+{
+  UP,
+  LEFT,
+  DOWN,
+  RIGHT
+} SPACE_ENUM;
 SPACE_ENUM space = UP;
 
 uint16_t b_color = COLOR(255, 255, 255);
 
-//Old
+// Old
 uint16_t adc = 0;
 typedef struct
-  {
-    uint8_t Parameter1;    // 1 byte
-    uint8_t Parameter2;    // 1 byte
-    uint8_t Parameter3;    // 1 byte
-    uint8_t Parameter4;    // 1 byte
+{
+  uint8_t Parameter1; // 1 byte
+  uint8_t Parameter2; // 1 byte
+  uint8_t Parameter3; // 1 byte
+  uint8_t Parameter4; // 1 byte
 
-  } tpSettings;
- 
+} tpSettings;
+
 tpSettings settings;
 
 /* USER CODE END PV */
@@ -121,69 +126,153 @@ static void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
-	
+
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
 static void screenSaver(void)
 {
   const uint16_t colorBg = COLOR(48, 207, 172);
-	LCD_Fill(colorBg);
+  LCD_Fill(colorBg);
   STRING_OUT("Snake", 140, 210, 3, 0x00FF, colorBg);
 }
 
 static void screenEndGame(void)
 {
   const uint16_t colorBg = COLOR(242, 65, 98);
-	LCD_Fill(colorBg);
+  LCD_Fill(colorBg);
   STRING_OUT("GAME OVER", 140, 210, 3, 0x00FF, colorBg);
 }
 
 static void screenGameCompleted(void)
 {
   const uint16_t colorBg = COLOR(43, 217, 46);
-	LCD_Fill(colorBg);
+  LCD_Fill(colorBg);
   STRING_OUT("Good game!", 140, 210, 3, 0x00FF, colorBg);
 }
 
 static void createFood(uint16_t x0, uint16_t y0)
 {
- const uint16_t radius = sizeFood;
- const uint16_t green = COLOR(0, 255, 0);
- fillCircle(x0, y0, radius, green);
+  const uint16_t radius = sizeFood;
+  const uint16_t green = COLOR(0, 255, 0);
+  fillCircle(x0, y0, radius, green);
 }
 
 static void createWalls(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1)
 {
- const uint16_t size = 4;
- const uint16_t blue = COLOR(42, 165, 184);
- H_line(x0, y0, x1, y1, size, blue);
+  const uint16_t size = 4;
+  const uint16_t blue = COLOR(42, 165, 184);
+  H_line(x0, y0, x1, y1, size, blue);
 }
 
-
-void up() 
+void up()
 {
-  changeX = 0;   //changes the direction of the snake
+  changeX = 0; // changes the direction of the snake
   changeY = -1;
 }
 
-void down() 
+void down()
 {
   changeX = 0;
   changeY = 1;
 }
 
-void left() 
+void left()
 {
   changeX = -1;
   changeY = 0;
 }
 
-void right() 
+void right()
 {
   changeX = 1;
   changeY = 0;
 }
+
+static void direction(void)
+{
+  switch (space)
+  {
+  case UP:
+    up();
+    break;
+  case LEFT:
+    left();
+    break;
+  case DOWN:
+    down();
+    break;
+  case RIGHT:
+    right();
+    break;
+
+  default:
+    space = UP;
+    break;
+  }
+}
+
+static void buttonRightHandler(void)
+{
+  static bool flagBut2 = false;
+
+  if ((HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) && !flagBut2)
+  { // обработчик нажатия
+    HAL_Delay(20);
+    if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET)
+    {
+      flagBut2 = true;
+
+      if (space == 0)
+      {
+        space = RIGHT;
+      }
+      space--;
+      direction();
+    }
+  }
+  if ((HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_SET) && flagBut2)
+  { // обработчик отпускания
+    flagBut2 = false;
+  }
+}
+
+static void buttonLeftHandler(void)
+{
+  static bool flagBut1 = false;
+
+  if ((HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_RESET) && !flagBut1)
+  { // обработчик нажатия
+    HAL_Delay(20);
+    if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_RESET)
+    {
+      flagBut1 = true;
+      space++;
+      if (space >= 4)
+      {
+        space = UP;
+      }
+      direction();
+    }
+  }
+  if ((HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14) == GPIO_PIN_SET) && flagBut1)
+  { // обработчик отпускания
+    flagBut1 = false;
+  }
+}
+#if 0
+		  HAL_ADC_Start(&hadc1);
+		  HAL_ADC_PollForConversion(&hadc1, 100);
+		  adc = HAL_ADC_GetValue(&hadc1);
+		  HAL_ADC_Stop(&hadc1);
+		  adc = adc/y_scale; // (развертка по У)
+
+      adc++;
+      if (adc > 200) 
+      {
+        adc = 0;
+      } // чтобы не вылазило за экран графика
+#endif
 /* USER CODE END 0 */
 
 int main(void)
@@ -210,19 +299,19 @@ int main(void)
   MX_USART1_UART_Init();
 
   /* USER CODE BEGIN 2 */
-	
-	HAL_Delay(75); // Добавим задержку, для исключения дребезга питания
-	LCD_Init();
-	LCD_setOrientation(ORIENTATION_LANDSCAPE_MIRROR);
-	
+
+  HAL_Delay(50); // Добавим задержку, для исключения дребезга питания
+  LCD_Init();
+  LCD_setOrientation(ORIENTATION_LANDSCAPE_MIRROR);
+
   screenSaver();
   HAL_Delay(950);
 
   /* Отрисуем рабочее поле */
-	LCD_Fill(0x0000);
-	line(0, 201, 319, 201, 0xFFFF);
-	line(0, 0, 0, 199, 0xFFFF);
-	STRING_OUT("Счет", 15, 210, 1, 0xFFFF, 0x0000);
+  LCD_Fill(0x0000);
+  line(0, 201, 319, 201, 0xFFFF);
+  line(0, 0, 0, 199, 0xFFFF);
+  STRING_OUT("Счет", 15, 210, 1, 0xFFFF, 0x0000);
 
   /* Отрисуем еду */
   createFood(xFood, yFood);
@@ -231,8 +320,8 @@ int main(void)
   createWalls(140, 180, 140, 20);
   createWalls(75, 180, 75, 20);
 
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-  
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+
   up();
   /* USER CODE END 2 */
 
@@ -240,117 +329,61 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-  /* USER CODE END WHILE */
+/* USER CODE END WHILE */
 
-  /* USER CODE BEGIN 3 */
-  #if 0
-		  HAL_ADC_Start(&hadc1);
-		  HAL_ADC_PollForConversion(&hadc1, 100);
-		  adc = HAL_ADC_GetValue(&hadc1);
-		  HAL_ADC_Stop(&hadc1);
-		  adc = adc/y_scale; // (развертка по У)
+/* USER CODE BEGIN 3 */
+    // LCD_fillRect(i+1, 0, 2, 200, 0x0000); // закрашиваем отрисованный график перед его перерисовкой новым графикомS
+    old_x = x_snake;
+    old_y = y_snake;
 
-      adc++;
-      if (adc > 200) 
-      {
-        adc = 0;
-      } // чтобы не вылазило за экран графика
-	#endif
+    y_snake = y_snake + changeY;
+    x_snake = x_snake + changeX;
 
-		  //LCD_fillRect(i+1, 0, 2, 200, 0x0000); // закрашиваем отрисованный график перед его перерисовкой новым графикомS
-		  old_x = x_snake;
-      old_y = y_snake;
+    if (y_snake > Y_MAX)
+    {
+      y_snake = Y_MIN;
+    }
+    if (x_snake > X_MAX)
+    {
+      x_snake = X_MIN;
+    }
+    if (y_snake < Y_MIN)
+    {
+      y_snake = Y_MAX;
+    }
+    if (x_snake < X_MIN)
+    {
+      x_snake = X_MAX;
+    }
 
-      y_snake = y_snake + changeY;
-      x_snake = x_snake + changeX;
-      
-      if (y_snake > Y_MAX)
-      {
-        y_snake = Y_MIN;
-      }
+    LCD_DrawPixel(x_snake, y_snake, 0XFFFF);
+    LCD_DrawPixel(old_x, old_y, 0X0000);
 
-      if (x_snake > X_MAX)
-      {
-        x_snake = X_MIN;
-      }
-
-      if (y_snake < Y_MIN)
-      {
-        y_snake = Y_MAX;
-      }
-
-      if (x_snake < X_MIN)
-      {
-        x_snake = X_MAX;
-      }
-
-      
-  static bool flag = false;
-
-  // читаем инвертированное значение для удобства
-  if ((HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET) && !flag) 
-  {  // обработчик нажатия
-      HAL_Delay(20);
-      if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_RESET)
-      {
-        flag = true;
-        space++;
-        if(space >= 4) space = UP;
-
-        switch (space)
-        {
-          case UP:
-            up();
-            break;
-          case LEFT:
-            left();
-            break;
-          case DOWN:
-            down();
-            break;
-          case RIGHT:
-            right();
-            break;
-        
-          default:
-          space = UP;
-          break;
-        }
-      }
-  }
-  if ((HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == GPIO_PIN_SET) && flag) 
-  {  // обработчик отпускания
-    flag = false;  
-  }
-
-
-		  LCD_DrawPixel(x_snake, y_snake, 0XFFFF);
-      LCD_DrawPixel(old_x, old_y, 0X0000);
-		
 #if DEBUG
-		  STRING_NUM_L(y_snake, 3, 120, 210, b_color, 0x0000);
-      STRING_NUM_L(x_snake, 3, 195, 210, b_color, 0x0000);
+    STRING_NUM_L(y_snake, 3, 120, 210, b_color, 0x0000);
+    STRING_NUM_L(x_snake, 3, 195, 210, b_color, 0x0000);
 #else
-
-      STRING_NUM_L(score, 3, 195, 210, b_color, 0x0000);
+    STRING_NUM_L(score, 3, 195, 210, b_color, 0x0000);
 #endif
-    if (((x_snake <= (xFood + sizeFood))&&(x_snake >= (xFood - sizeFood))) \
-    && ((y_snake <= (yFood + sizeFood))&&(y_snake >= (yFood - sizeFood))))
-    { //food
+    if (((x_snake <= (xFood + sizeFood)) && (x_snake >= (xFood - sizeFood))) && ((y_snake <= (yFood + sizeFood)) && (y_snake >= (yFood - sizeFood))))
+    { // food
       screenGameCompleted();
-      HAL_Delay(5000);
+      HAL_Delay(2000);
+      LCD_SendCommand(LCD_SWRESET);
+      HAL_Delay(2000);
       HAL_NVIC_SystemReset();
     }
+
+    buttonLeftHandler();
+    buttonRightHandler();
+
     HAL_Delay(TIME_UPDATE); // (развертка по Х)
-	
-		
   }
   /* USER CODE END 3 */
-
 }
 
 /** System Clock Configuration
-*/
+ */
 void SystemClock_Config(void)
 {
 
@@ -358,8 +391,8 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
+  /**Initializes the CPU, AHB and APB busses clocks
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -372,10 +405,9 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  /**Initializes the CPU, AHB and APB busses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -393,16 +425,16 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-    /**Enables the Clock Security System 
-    */
+  /**Enables the Clock Security System
+   */
   HAL_RCC_EnableCSS();
 
-    /**Configure the Systick interrupt time 
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+  /**Configure the Systick interrupt time
+   */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
 
-    /**Configure the Systick 
-    */
+  /**Configure the Systick
+   */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
   /* SysTick_IRQn interrupt configuration */
@@ -415,8 +447,8 @@ static void MX_ADC1_Init(void)
 
   ADC_ChannelConfTypeDef sConfig;
 
-    /**Common config 
-    */
+  /**Common config
+   */
   hadc1.Instance = ADC1;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
@@ -429,8 +461,8 @@ static void MX_ADC1_Init(void)
     Error_Handler();
   }
 
-    /**Configure Regular Channel 
-    */
+  /**Configure Regular Channel
+   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = 1;
   sConfig.SamplingTime = ADC_SAMPLETIME_7CYCLES_5;
@@ -438,7 +470,6 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
-
 }
 
 /* SPI1 init function */
@@ -461,7 +492,6 @@ static void MX_SPI1_Init(void)
   {
     Error_Handler();
   }
-
 }
 
 /* TIM2 init function */
@@ -493,7 +523,6 @@ static void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-
 }
 
 /* USART1 init function */
@@ -512,13 +541,12 @@ static void MX_USART1_UART_Init(void)
   {
     Error_Handler();
   }
-
 }
 
-/** 
-  * Enable DMA controller clock
-  */
-static void MX_DMA_Init(void) 
+/**
+ * Enable DMA controller clock
+ */
+static void MX_DMA_Init(void)
 {
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
@@ -527,16 +555,15 @@ static void MX_DMA_Init(void)
   /* DMA1_Channel3_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
-
 }
 
-/** Configure pins as 
-        * Analog 
-        * Input 
-        * Output
-        * EVENT_OUT
-        * EXTI
-*/
+/** Configure pins as
+ * Analog
+ * Input
+ * Output
+ * EVENT_OUT
+ * EXTI
+ */
 static void MX_GPIO_Init(void)
 {
 
@@ -561,13 +588,13 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA1 PA2 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2;
+  GPIO_InitStruct.Pin = GPIO_PIN_1 | GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB12 PB13 PB14 PB15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -577,7 +604,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
@@ -585,46 +611,45 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @param  None
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler */
   /* User can add his own implementation to report the HAL error return state */
-  while(1) 
+  while (1)
   {
   }
-  /* USER CODE END Error_Handler */ 
+  /* USER CODE END Error_Handler */
 }
 
 #ifdef USE_FULL_ASSERT
 
 /**
-   * @brief Reports the name of the source file and the source line number
-   * where the assert_param error has occurred.
-   * @param file: pointer to the source file name
-   * @param line: assert_param error line source number
-   * @retval None
-   */
-void assert_failed(uint8_t* file, uint32_t line)
+ * @brief Reports the name of the source file and the source line number
+ * where the assert_param error has occurred.
+ * @param file: pointer to the source file name
+ * @param line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
-
 }
 
 #endif
 
 /**
-  * @}
-  */ 
+ * @}
+ */
 
 /**
-  * @}
-*/ 
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
